7a8d1e77b2aa0de0856178bd1de054660c2f2d57
John Martin <jcmartin@bu.edu>
2013-03-23 14:15:16 -0400

    Final maze files added to main library

--- Assignment 3/lib/maze.h
+++ Assignment 3/lib/maze.h
@@ -5,54 +5,54 @@
 //extern Maze maze; 
 
 typedef struct {
-  int x;
-  int y;
+	int x;
+	int y;
 }Position;
 
 typedef enum{
-  CT_Floor =' ',
-  CT_Wall = '#',
-  CT_Jailj ='j',
-  CT_JailJ ='J',
-  CT_Homeh ='h',
-  CT_HomeH ='H',
+	CT_Floor =' ',
+	CT_Wall = '#',
+	CT_Jailj ='j',
+	CT_JailJ ='J',
+	CT_Homeh ='h',
+	CT_HomeH ='H',
 }Cell_Type;
 
 typedef enum{
-  Team1=1,
-  Team2=2
+	Team1=1,
+	Team2=2
 }Team;
 
 typedef struct{
-  Team team;
-  Position PlayerPos;
-  int ID;
-  int PID;
-  // Object *o;  //fix for items
+	Team team;
+	Position PlayerPos;
+	int ID;
+	int PID;
+	// Object *o;  //fix for items
 }Player;
 
 /* update for player list
-typedef struct{
-  Player players[numPlayers];
-}PlayerList;
-*/
+ typedef struct{
+ Player players[numPlayers];
+ }PlayerList;
+ */
 
 typedef struct{
-  Cell_Type C_Type;
-  Position Cell_Pos;
-  Player *p;
-  Team Cell_Team;
-  //Object *o;  //fix for items
+	Cell_Type C_Type;
+	Position Cell_Pos;
+	Player *p;
+	Team Cell_Team;
+	//Object *o;  //fix for items
 }Cell;
 
 typedef struct {
-  Cell *cells;
-  int numFloor;
-  int numWall;
-  int numCells;
-  int numOfJails[2];
-  int numOfHomes[2];
-  Position dimensions;
+	Cell *cells;
+	int numFloor;
+	int numWall;
+	int numCells;
+	int numOfJails[2];
+	int numOfHomes[2];
+	Position dimensions;
 }Maze;
 
 extern int  findNumHome(int team);
