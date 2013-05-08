86790d3ff355118e5c1ef8b7ee6b55425254cf97
John Martin <jcmartin@bu.edu>
2013-05-08 02:43:22 -0400

    UI

---
+++ UI/lib/client_types.h
@@ -0,0 +1,35 @@
+
+#ifndef CLIENT_TYPES_H
+#define CLIENT_TYPES_H
+
+/* Game Client Variables */
+
+typedef struct {
+   int game_state;
+   unsigned long long game_version;
+} GameData;
+
+typedef struct {
+   int rows;
+   int columns;
+   char* maze;
+   unsigned long long *cell_version;
+} GameMaze;
+
+typedef struct {
+   int id;
+   int team;
+   int x;
+   int y;
+   char state;
+   char flag;
+   char shovel;
+} PlayerInfo;
+
+extern GameData gamedata;
+extern GameMaze themaze;
+extern PlayerInfo playerdata;
+
+extern pthread_mutex_t client_data_mutex;
+extern pthread_mutex_t client_maze_mutex;
+#endif
